d1c5e514cb5acfe3f24e681e56f92e6190af8c27
blancocd_l13yoga <blancocedi@gmail.com>
2024-01-09 15:56:14 +0100

    finished happiness score variance study, changed paths for new organization of files

--- exp/exp_CLM_001_DataCleaningSuicideRates.py
+++ exp/exp_CLM_001_DataCleaningSuicideRates.py
@@ -8,7 +8,7 @@ Created on Tue Jan  9 13:37:57 2024
 import pandas as pd
 
 # Load the suicide rate dataset
-data = pd.read_csv('Age-Standardized-Suicide-Rate-Data.csv')
+data = pd.read_csv('../dat/Age-Standardized-Suicide-Rate-Data.csv')
 
 # Create a new dataset with countries as rows, years as columns, and suicide rates as values
 # There will be three datasets, one for suicide rates of both sexes combined, one for suicide rates of males and another one for females
